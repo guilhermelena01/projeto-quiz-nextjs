1ab3537666c4078b25d069e63b99e01e8b273b13
Guilherme Lena <guilhermelena@lenatech.com.br>
2024-01-03 23:27:44 -0300

    inserindo componente botao

--- components/Resposta.tsx
+++ components/Resposta.tsx
@@ -15,29 +15,34 @@ export default function Resposta(props: Respostaprops) {
     return (
         <div className={styles.resposta} onClick={() => { props.respostaFornecida(props.indice) }}>
             <div className={styles.conteudoResposta}>
-                {/* <div className={styles.frente}>
-                    <div className={styles.letra}
-                        style={{ backgroundColor: props.corFundoLetra }}
-                    >
-                        {props.letra}
-                    </div>
-                    <div className={styles.valor}>
-                        {resposta.valor}
-                    </div>
-                </div> */}
-                <div className={styles.verso}>
-                    {resposta.certa ? (
-                        <div className={styles.certa}>
-                            <div>A resposta certa é...</div>
-                            <div className={styles.valor}>{resposta.valor}</div>
-                        </div> ) : (
-                        <div className={styles.errada}>
-                            <div>A resposta informada está incorreta...</div>
-                            <div className={styles.valor}>{resposta.valor}</div>
+                {!resposta.revelada ? (
+                    <div className={styles.frente}>
+                        <div className={styles.letra}
+                            style={{ backgroundColor: props.corFundoLetra }}
+                        >
+                            {props.letra}
                         </div>
-                    )
-                    }
-                </div>
+                        <div className={styles.valor}>
+                            {resposta.valor}
+                        </div>
+                    </div>
+                ) : (
+                    <div className={styles.verso}>
+                        {resposta.certa ? (
+                            <div className={styles.certa}>
+                                <div>A resposta certa é...</div>
+                                <div className={styles.valor}>{resposta.valor}</div>
+                            </div>) : (
+                            <div className={styles.errada}>
+                                <div>A resposta informada está incorreta...</div>
+                                <div className={styles.valor}>{resposta.valor}</div>
+                            </div>
+                        )
+                        }
+                    </div>
+                )
+
+                }
             </div>
         </div>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
+import Botao from "@/components/Botao";
 import Questao from "@/components/Questao";
 import QuestaoModel from "@/model/questao";
 import RespostasModel from "@/model/respostas";
 import { useState } from "react";
 
-const questaoMock = 
+const questaoMock =
     new QuestaoModel(1, 'Melhor cor?', [
         RespostasModel.errada('Verde'),
         RespostasModel.errada('Vermelha'),
@@ -15,20 +16,30 @@ export default function Home() {
 
     const [questao, setQuestao] = useState(questaoMock)
 
-    function respostaFornecida (indice:number) {
+
+    function respostaFornecida(indice: number) {
         console.log(indice);
         setQuestao(questao.responderCom(indice))
-        
+    }
+    function tempoEsgotado() {
+        if (questao.naoRespondida) {
+            setQuestao(questao.responderCom(-1))
+        }
     }
     return (
         <div style={{
             display: 'flex',
+            flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'center',
             height: '100vh',
         }}>
             <Questao valor={questao}
-            respostaFornecida={respostaFornecida} />
+                tempoPraResposta={5}
+                respostaFornecida={respostaFornecida}
+                tempoEsgotado={tempoEsgotado}
+            />
+            <Botao texto="Teste" />
         </div>
 
     )
